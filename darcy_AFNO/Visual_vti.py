e6ab24035265685fa86a0c29e5d0d076e8d04c1d
orangebabyb <81520258+orangebabyb@users.noreply.github.com>
2025-07-01 18:41:39 +0800

    Update Visual_vti.py

--- darcy_AFNO/Visual_vti.py
+++ darcy_AFNO/Visual_vti.py
@@ -1,17 +1,15 @@
 import pyvista as pv
 
-# 讀檔
-#grid = pv.read("outputs/darcy_AFNO/validators/test.vti")
+# read vti file
 grid = pv.read("outputs/custom_darcy_AFNO/validators/test_0.vti")
 
-# 印出完整資訊（含結構、陣列名稱、資料型態等）
+# print dict struct
 print(grid)
 
-# 印出所有欄位（也就是每個變數/通道）
+# print all column
 print("\n[欄位名稱] Available arrays:", grid.point_data.keys())
 
-# 印出特定欄位的統計資訊（例如 true_sol)
-print("【所有欄位名稱與資料型態】")
+print("[所有欄位名稱與資料型態]")
 for name in grid.point_data.keys():
     arr = grid[name]
     print(f"{name}:{arr}")
