0fb314c4b2fb6b2f070eede659ab95c7c8faa893
Orange baby <81520258+orangebabyb@users.noreply.github.com>
2025-07-28 21:51:10 +0800

    Delete PINN_class_week2 directory

--- PINN_class_week2/custom_node_sympy.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-from sympy import Symbol, sin
-from physicsnemo.sym.node import Node
-
-node = Node.from_sympy(sin(Symbol("x")), "sin_x")
-
-result = node.evaluate({"x": (torch.ones(10, 1))*np.pi/4,})
-print(result)
--- PINN_class_week2/show_pde.py
+++
@@ -1,3 +0,0 @@
-from physicsnemo.sym.eq.pdes.navier_stokes import NavierStokes
-ns = NavierStokes(nu=0.01, rho=1, dim=2)
-ns.pprint()
